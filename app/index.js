13d5936dd9130c073425073d9f2ab42d62cc2e2e
Damian Silbergleith Cunniff <dcunniff@gmail.com>
2019-08-28 07:33:26 -0700

    setup the powerbar so it's all CSS and can be move easily.

--- app/index.js
+++ app/index.js
@@ -3,21 +3,37 @@ import document from "document";
 document.onkeypress = (e) => {
 
   if (e.key === "up") {
-    const powerMask = document.getElementById('power-mask-img');
+    const powerMask = document.getElementById('power-mask').getElementsByTagName("rect")[0];
     console.log(JSON.stringify(powerMask, null, 2));
 
-    const START = 100;
+    const START = 130;
     const MAX = 300;
-    const STEP = 20;
+    const STEP = 18;
 
     console.log(`up`);
     e.preventDefault();
-    if (powerMask.width >= MAX) {
-      powerMask.width = START;
-    } else {
-      powerMask.width += STEP;
-    }
-    e.preventDefault();
+
+    let iter = 0;
+    powerMask.width = START;
+    const f = setInterval(() => {
+      let step = STEP;
+      if (iter >= 8) {
+       clearInterval(f);
+       powerMask.width = 0;
+       return;
+      }
+      if (iter >= 6) {
+        step = 27;
+      }
+
+      if (powerMask.width >= MAX) {
+        powerMask.width = START;
+      } else {
+        powerMask.width += step;
+      }
+      iter += 1;
+    }, 1000);
+
 
   }
   if (e.key === "down") {
