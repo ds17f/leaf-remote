dba395bbec0d6fa3c4f62aae2ea8dd2d63c6125e
Damian Silbergleith Cunniff <dcunniff@gmail.com>
2019-08-28 12:57:21 -0700

    POC only run one powerUp at a time

--- app/index.js
+++ app/index.js
@@ -1,39 +1,48 @@
 import document from "document";
 
+const isRunning = null;
+const powerUp = () => {
+  const powerMask = document.getElementById('power-mask').getElementsByTagName("rect")[0];
+
+  const START = 130;
+  const MAX = 300;
+  const STEP = 18;
+
+  let iter = 0;
+  powerMask.width = START;
+  const f = setInterval(() => {
+    let step = STEP;
+    if (iter >= 8) {
+      clearInterval(f);
+      powerMask.width = 0;
+      isRunning = null;
+      return;
+    }
+    if (iter >= 6) {
+      step = 27;
+    }
+
+    if (powerMask.width >= MAX) {
+      powerMask.width = START;
+    } else {
+      powerMask.width += step;
+    }
+    iter += 1;
+  }, 1000);
+
+  return f;
+};
+
 document.onkeypress = (e) => {
-
   if (e.key === "up") {
-    const powerMask = document.getElementById('power-mask').getElementsByTagName("rect")[0];
-    console.log(JSON.stringify(powerMask, null, 2));
-
-    const START = 130;
-    const MAX = 300;
-    const STEP = 18;
-
     console.log(`up`);
     e.preventDefault();
-
-    let iter = 0;
-    powerMask.width = START;
-    const f = setInterval(() => {
-      let step = STEP;
-      if (iter >= 8) {
-       clearInterval(f);
-       powerMask.width = 0;
-       return;
-      }
-      if (iter >= 6) {
-        step = 27;
-      }
-
-      if (powerMask.width >= MAX) {
-        powerMask.width = START;
-      } else {
-        powerMask.width += step;
-      }
-      iter += 1;
-    }, 1000);
-
+    if ( ! isRunning ) {
+      isRunning = powerUp();
+      console.log("start running")
+    } else {
+      console.log("already running")
+    }
 
   }
   if (e.key === "down") {
