952bc07d601aad66092c8e36fd06df0a57506f5e
Damian Silbergleith Cunniff <dcunniff@gmail.com>
2019-08-31 22:36:29 -0700

    send a message when woken by peer connect

--- companion/index.js
+++ companion/index.js
@@ -1,13 +1,24 @@
-import * as messaging from "messaging";
+import { peerSocket } from "messaging";
+import { me } from "companion"
+
+if (me.launchReasons.peerAppLaunched) {
+  // The Device application caused the Companion to start
+  console.log("Device application was launched!");
+  try {
+    peerSocket.send("I hear you.");
+  } catch (error) {
+    console.log(`couldn't send message: ${error}`)
+  }
+}
 
 // Listen for the onmessage event
-messaging.peerSocket.onmessage = function(evt) {
+peerSocket.onmessage = (evt) => {
   // Output the message to the console
   console.log(JSON.stringify(evt.data));
 };
 
 // Listen for the onopen event
-messaging.peerSocket.onopen = () => {
+peerSocket.onopen = () => {
   // Ready to send or receive messages
   console.log("Ready to send/receive")
 };
